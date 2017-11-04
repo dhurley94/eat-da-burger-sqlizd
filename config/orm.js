4c26fe9eb9864740f10cccc2ce460aefdedd236e
dhurley94 <dhurley@singlehop.com>
2017-11-04 07:45:09 -0400

    installed sequelized and began refactoring orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,36 @@
+'use strict';
+
 const connect = require('./connection');
 const sqlize = require('sequelize');
 
-// module.exports = {
-//     /**
-//      * Get all data from `burgers` tbl
-//      * return array ordered by id
-//      */
+module.exports = {
+        /**
+         * Get all data from `burgers` tbl
+         * return array ordered by id
+         */
+        selectAll: (cb, 'description') => {
+            sqlize.findAll(cb) {
+                return cb(result);
+            }
+        },
+        /**
+         * INSERTS data from form into `burgers` tbl
+         * takes one param for burger name
+         */
+        insertOne: (bg_name) => {
+
+        },
+        /**
+         * Updates `burgers` tbl based on id
+         * takes two params
+         * burger id and new `devoured` data
+         */
+        updateOne: (_id, _devoured) => {
+
+        }
+    }
+    // module.exports = {
+
 //     selectAll: (cb) => {
 //         connect.execute('SELECT * FROM burgers ORDER BY id', (error, result) => {
 //             if (error) { return console.log(error); }
