dd09a88679f2a58900e824ffe1a497c9cf20d1a2
dhurley94 <drhurley94@gmail.com>
2017-11-12 12:55:08 -0500

    removed seed and added comments for Heather

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
     var Burger = sequelize.define("burger", {
+        /**
+         * Instantiates burger and verifies that input is not empty
+         * Using the native sequelize validate command
+         */
         burger_name: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -7,11 +11,18 @@ module.exports = function(sequelize, DataTypes) {
                 notEmpty: true
             }
         },
+        /**
+         * Creates `devoured` row
+         * Used to determine if burger has been eaten
+         */
         devoured: {
             type: DataTypes.BOOLEAN,
             allowNull: false,
             defaultValue: 0
         },
+        /**
+         * Not actively used, but exists for future iterations
+         */
         active: {
             type: DataTypes.BOOLEAN,
             allowNull: false,
